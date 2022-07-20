f2fdfa761357be8c47616c1757bf1f99ed36c5de
Hud Yusuf Atsigah <hudjempol@gmail.com>
2022-07-20 18:17:28 +0800

    try

--- index.js
+++ index.js
@@ -65,10 +65,23 @@ app.post(
   "/api/newAbsen",
   catchAsync(async (req, res) => {
     const { peserta, sesi } = req.body;
-    const time = new Date().toLocaleTimeString();
+    const time = new Date().getHours();
     const tgl = new Date().getDate();
     const bln = new Date().getMonth();
     const thn = new Date().getFullYear();
+
+    function convertSeconds(seconds) {
+      var convert = function (x) {
+        return x < 10 ? "0" + x : x;
+      };
+      return (
+        convert(parseInt(seconds / (60 * 60))) +
+        ":" +
+        convert(parseInt((seconds / 60) % 60)) +
+        ":" +
+        convert(seconds % 60)
+      );
+    }
 
     const checkAbsen = await Absen.findOne({ tgl, bln, thn, peserta, sesi });
 
@@ -103,7 +116,7 @@ app.use((err, req, res, next) => {
     .json({ error: err.message } || "something went wrong");
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("listening on port" + port);
 });
