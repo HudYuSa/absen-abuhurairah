fb14571dfc90a2ce83e372c89694129de31ec0cd
Hud Yusuf Atsigah <hudjempol@gmail.com>
2022-07-20 18:54:25 +0800

    converting time to wita time

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 const Absen = require("./models/absen");
 
 const catchAsync = require("./catchAsync");
+const convertTZ = require("./convertTime");
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(
@@ -30,33 +31,27 @@ app.use(express.static("public"));
 app.get(
   "/",
   catchAsync(async (req, res, next) => {
-    // check if the request has an api key
-    const apiKey = req.query.apiKey;
-
-    if (apiKey === process.env.API_KEY) {
-      const filterCount = Object.keys(req.query).length;
-      // check if there's a filter query
-      if (filterCount) {
-        const { tgl, bln, thn, sesi } = req.query;
-        const filter = {};
-        tgl ? (filter.tgl = tgl) : null;
-        bln ? (filter.bln = bln) : null;
-        thn ? (filter.thn = thn) : null;
-        sesi ? (filter.sesi = sesi) : null;
-        const allAbsen = await Absen.find(filter);
-        const sortedAbsen = allAbsen.sort(function (a, b) {
-          return a.peserta === b.peserta ? 0 : a.peserta < b.peserta ? -1 : 1;
-        });
-        return res.render("homePage", { allAbsen: sortedAbsen });
-      }
-
-      const allAbsen = await Absen.find({});
+    const filterCount = Object.keys(req.query).length;
+    // check if there's a filter query
+    if (filterCount) {
+      const { tgl, bln, thn, sesi } = req.query;
+      const filter = {};
+      tgl ? (filter.tgl = tgl) : null;
+      bln ? (filter.bln = bln) : null;
+      thn ? (filter.thn = thn) : null;
+      sesi ? (filter.sesi = sesi) : null;
+      const allAbsen = await Absen.find(filter);
       const sortedAbsen = allAbsen.sort(function (a, b) {
         return a.peserta === b.peserta ? 0 : a.peserta < b.peserta ? -1 : 1;
       });
       return res.render("homePage", { allAbsen: sortedAbsen });
     }
-    next(new Error("you're not authorized"));
+
+    const allAbsen = await Absen.find({});
+    const sortedAbsen = allAbsen.sort(function (a, b) {
+      return a.peserta === b.peserta ? 0 : a.peserta < b.peserta ? -1 : 1;
+    });
+    return res.render("homePage", { allAbsen: sortedAbsen });
   })
 );
 
@@ -64,35 +59,42 @@ app.get(
 app.post(
   "/api/newAbsen",
   catchAsync(async (req, res) => {
-    const { peserta, sesi } = req.body;
-    const time = new Date().toLocaleTimeString("ID");
-    const tgl = new Date().getDate();
-    const bln = new Date().getMonth();
-    const thn = new Date().getFullYear();
+    // check if the request has an api key
+    const apiKey = req.query.apiKey;
 
-    const checkAbsen = await Absen.findOne({ tgl, bln, thn, peserta, sesi });
+    if (apiKey === process.env.API_KEY) {
+      const { peserta, sesi } = req.body;
+      const indoDate = convertTZ();
 
-    if (checkAbsen) {
-      return res.status(400).send("peserta sudah hadir");
+      const time = indoDate.toLocaleTimeString();
+      const tgl = new Date().getDate();
+      const bln = new Date().getMonth();
+      const thn = new Date().getFullYear();
+
+      const checkAbsen = await Absen.findOne({ tgl, bln, thn, peserta, sesi });
+
+      if (checkAbsen) {
+        return res.status(400).send("peserta sudah hadir");
+      }
+      const newAbsen = new Absen({
+        peserta,
+        sesi,
+        time,
+        tgl,
+        bln,
+        thn,
+      });
+
+      await newAbsen.save();
+
+      return res.json({ success: "Berhasil" });
     }
-    const newAbsen = new Absen({
-      peserta,
-      sesi,
-      time,
-      tgl,
-      bln,
-      thn,
-    });
-
-    await newAbsen.save();
-
-    res.json({ success: "Berhasil" });
+    next(new Error("you're not authorized"));
   })
 );
-
 // handle error
 // if there are no route with all method
-app.all("*", (req, res, next) => {
+app.use("*", (req, res, next) => {
   next(new Error("Page not found"));
 });
 
--- models/absen.js
+++ models/absen.js
@@ -1,9 +1,10 @@
 const { Schema, model } = require("mongoose");
+const convertTZ = require("../convertTime");
 
 const absenSchema = new Schema({
   peserta: { type: String, required: true },
   sesi: { type: Number, min: 1, required: true },
-  time: { type: String, default: new Date().toLocaleTimeString("ID") },
+  time: { type: String, default: convertTZ() },
   tgl: { type: String, default: new Date().getDate() },
   bln: { type: String, default: new Date().getMonth() },
   thn: { type: String, default: new Date().getFullYear() },
